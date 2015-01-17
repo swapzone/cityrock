d1d015fb802a040d1fadd89b54f40888e36f0638
Sascha Gros <gros.sascha@gmail.com>
2015-01-17 14:59:32 +0100

    Refactor REST API and create basic function structure

--- site/index.php
+++ site/index.php
@@ -14,49 +14,7 @@ if(strpos($url, "/api") === 0) {
 	/***********************************************************************/
 	/* REST Api called, instantiate Slim framework											   */
 	/***********************************************************************/
-	require_once 'lib/Slim/Slim.php';
-	require_once('_api.php');
-
-	\Slim\Slim::registerAutoloader();
-
-	$app = new \Slim\Slim(array(
-		'debug' => true
-	));
-
-	// configure response
-	$response = $app->response();
-	$response->header('content-type', 'application/json');
-
-	// API group
-	$app->group('/api', function () use ($app) {
-
-		$app->get('/course/:type/list', function ($type) {
-			echo getCoursesJSON($type);
-		});
-
-		$app->get('/course/:type/:id', function ($type, $id) {
-			echo "Course of type $type with ID: $id";
-		});
-
-		$app->get('/course/:id', function ($id) {
-			echo getCourseJSON($id);
-		});
-
-		$app->get('/config', function () {	
-			echo 'Configuration object';
-		});
-
-		$app->get('/registrants/list', function() {
-			// set response header to pdf
-			echo 'Registrants list';
-		});
-
-		$app->post('/registrant', function() {
-			// add registrant to database and return confirmation key
-		});
-	});
-
-	$app->run();
+	include_once('_api.php');
 }
 else {
 	/***********************************************************************/
